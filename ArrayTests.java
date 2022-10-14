fc990a747beda7accad2d4d977cfcecae948965c
Noah Kil <33174613+quantiumtech@users.noreply.github.com>
2022-10-13 18:32:36 -0700

    test

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,29 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+@Test
+public void testReversed2() {
+  int[] input = { 3, 6, 9 ,12};
+  int[] expected = {12, 9, 6, 3};
+ 
+  assertArrayEquals(expected, ArrayExamples.reversed(input));
+}
+
+@Test
+public void testReverseInPlace2() {
+  int[] input = {3, 6, 9, 12};
+  int[] expected = {12, 9, 6, 3};
+  ArrayExamples.reverseInPlace(input);
+  assertArrayEquals(expected, input);
+}
+
+@Test
+public void testAverageWithoutLowest() {
+double[] input = {3.0, 3.0, 7.0};
+double expected = 5.0;
+double output = ArrayExamples.averageWithoutLowest(input);
+assertEquals(expected, output, .0001);
+}
 }
